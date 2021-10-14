47c7a4ec107b2a2159fd5086b52e2bcfaadc01c6
makrker <86200419+makrker@users.noreply.github.com>
2021-10-14 15:57:05 +0700

    Update ada.sh

--- ada.sh
+++ ada.sh
@@ -8,7 +8,7 @@ sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
 sudo pacman-key --lsign-key 3056513887B78AEB
 sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
 
-sudo  echo '[chaotic-aur]
+sudo echo '[chaotic-aur]
 Include = /etc/pacman.d/chaotic-mirrorlist ' >> /etc/pacman.conf
 sudo pacman -Sy
 git clone https://aur.archlinux.org/paru.git 
