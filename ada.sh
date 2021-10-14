ddd4d89592606ccf57770fb88761b0c3485dc0fd
makrker <86200419+makrker@users.noreply.github.com>
2021-10-14 16:17:32 +0700

    Update ada.sh

--- ada.sh
+++ ada.sh
@@ -6,7 +6,7 @@ sudo pacman -S base-devel wget unzip --noconfirm
 
 sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
 sudo pacman-key --lsign-key 3056513887B78AEB
-sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst' --noconfirm
 cd 
 touch ld 
 echo '[chaotic-aur]
