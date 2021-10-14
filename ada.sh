0e52087cefb34d116481d2e3f53bc20710d2fb80
makrker <86200419+makrker@users.noreply.github.com>
2021-10-14 16:15:57 +0700

    Update ada.sh

--- ada.sh
+++ ada.sh
@@ -1,5 +1,5 @@
 sudo pacman -Sy 
-sudo pacman -S base-devel wget unzip 
+sudo pacman -S base-devel wget unzip --noconfirm
 
 
 
@@ -7,9 +7,12 @@ sudo pacman -S base-devel wget unzip
 sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
 sudo pacman-key --lsign-key 3056513887B78AEB
 sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+cd 
+touch ld 
+echo '[chaotic-aur]
+Include = /etc/pacman.d/chaotic-mirrorlist' >> ld 
+sudo tee -a /etc/pacman.conf < ld
 
-sudo sed -i '/custompkgs/[chaotic-aur]
-Include = /etc/pacman.d/chaotic-mirrorlist' /etc/pacman.conf
 sudo pacman -Sy
 git clone https://aur.archlinux.org/paru.git 
 cd paru 
@@ -51,7 +54,7 @@ cd
 
 
 
-wget https://raw.githubusercontent.com/logico-dev/Xresources-themes/master/base16-google-dark.Xresources .Xresources
+wget https://raw.githubusercontent.com/logico-dev/Xresources-themes/master/base16-google-dark.Xresources 
 
 sudo mv base16-google-dark.Xresources .Xresources
 
