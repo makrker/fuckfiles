8584882d2a0c6b543a051645acd4b94319884c46
makrker <86200419+makrker@users.noreply.github.com>
2021-11-08 08:08:26 +0700

    Update ada.sh

--- ada.sh
+++ ada.sh
@@ -10,6 +10,8 @@ sudo rm -rf /etc/pacman.d/mirrorlist
 sudo wget https://raw.githubusercontent.com/makrker/fuckfiles/main/do 
 sudo wget https://raw.githubusercontent.com/makrker/fuckfiles/main/pac
 sudo wget https://raw.githubusercontent.com/makrker/fuckfiles/main/spo
+sudo wget https://raw.githubusercontent.com/makrker/fuckfiles/main/as
+sudo mv as /etc/pacman.d/chaotic-mirrorlist
 sudo mv do /etc/pacman.d/mirrorlist 
 sudo mv pac /etc/pacman.conf
 sudo mv spo /usr/share/applications/spot.desktop
