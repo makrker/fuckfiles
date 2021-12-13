7c5d976c0c16bae4e7db7bd7ed416e9773d68928
makrker <86200419+makrker@users.noreply.github.com>
2021-12-13 09:43:11 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -13,7 +13,7 @@ sudo wget https://raw.githubusercontent.com/makrker/fuckfiles/main/pac
 sudo wget https://raw.githubusercontent.com/makrker/fuckfiles/main/spo
 sudo wget https://raw.githubusercontent.com/makrker/fuckfiles/main/as
 sudo mv as /etc/pacman.d/chaotic-mirrorlist
-sudo mv do /etc/pacman.d/mirrorlist 
+sudo mv ass /etc/pacman.d/mirrorlist 
 sudo mv pac /etc/pacman.conf
 sudo mv spo /usr/share/applications/spot.desktop
 sudo chmod +x /usr/share/applications/spot.desktop
