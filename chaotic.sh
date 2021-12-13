83905f045e0e31c4ba0c28fc6fc64da64d17bc78
makrker <86200419+makrker@users.noreply.github.com>
2021-12-13 17:53:28 +0700

    Update chaotic.sh

--- chaotic.sh
+++ chaotic.sh
@@ -1,6 +1,139 @@
 #!/bin/sh
 sudo pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
+sudo pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
+sudo pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
+sudo pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
+sudo pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
+sudo pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
+sudo pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
+sudo pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
+
 
 sudo pacman-key --lsign-key FBA220DFC880C03
+sudo pacman-key --lsign-key FBA220DFC880C03
+sudo pacman-key --lsign-key FBA220DFC880C03
+sudo pacman-key --lsign-key FBA220DFC880C03
+
+
+
+sudo pacman-key --lsign-key FBA220DFC880C03
+sudo pacman-key --lsign-key FBA220DFC880C03
+sudo pacman-key --lsign-key FBA220DFC880C03
+sudo pacman-key --lsign-key FBA220DFC880C03
+
+
+sudo pacman-key --lsign-key FBA220DFC880C03
+sudo pacman-key --lsign-key FBA220DFC880C03
+sudo pacman-key --lsign-key FBA220DFC880C03
+sudo pacman-key --lsign-key FBA220DFC880C03
+
+
+sudo pacman-key --lsign-key FBA220DFC880C03
+sudo pacman-key --lsign-key FBA220DFC880C03
+sudo pacman-key --lsign-key FBA220DFC880C03
+sudo pacman-key --lsign-key FBA220DFC880C03
+
+
+sudo pacman-key --lsign-key FBA220DFC880C03
+sudo pacman-key --lsign-key FBA220DFC880C03
+sudo pacman-key --lsign-key FBA220DFC880C03
+sudo pacman-key --lsign-key FBA220DFC880C03
+
+
+sudo pacman-key --lsign-key FBA220DFC880C03
+sudo pacman-key --lsign-key FBA220DFC880C03
+sudo pacman-key --lsign-key FBA220DFC880C03
+sudo pacman-key --lsign-key FBA220DFC880C03
+
+
+sudo pacman-key --lsign-key FBA220DFC880C03
+sudo pacman-key --lsign-key FBA220DFC880C03
+sudo pacman-key --lsign-key FBA220DFC880C03
+sudo pacman-key --lsign-key FBA220DFC880C03
+
+
+sudo pacman-key --lsign-key FBA220DFC880C03
+sudo pacman-key --lsign-key FBA220DFC880C03
+sudo pacman-key --lsign-key FBA220DFC880C03
+sudo pacman-key --lsign-key FBA220DFC880C03
+
+
+sudo pacman-key --lsign-key FBA220DFC880C03
+sudo pacman-key --lsign-key FBA220DFC880C03
+sudo pacman-key --lsign-key FBA220DFC880C03
+sudo pacman-key --lsign-key FBA220DFC880C03
+
+
+sudo pacman-key --lsign-key FBA220DFC880C03
+sudo pacman-key --lsign-key FBA220DFC880C03
+sudo pacman-key --lsign-key FBA220DFC880C03
+sudo pacman-key --lsign-key FBA220DFC880C03
+
+
+
+
+sudo pacman-key --lsign-key FBA220DFC880C03
+sudo pacman-key --lsign-key FBA220DFC880C03
+sudo pacman-key --lsign-key FBA220DFC880C03
+sudo pacman-key --lsign-key FBA220DFC880C03
+
+
+sudo pacman-key --lsign-key FBA220DFC880C03
+sudo pacman-key --lsign-key FBA220DFC880C03
+sudo pacman-key --lsign-key FBA220DFC880C03
+sudo pacman-key --lsign-key FBA220DFC880C03
+
+
+sudo pacman-key --lsign-key FBA220DFC880C03
+sudo pacman-key --lsign-key FBA220DFC880C03
+sudo pacman-key --lsign-key FBA220DFC880C03
+sudo pacman-key --lsign-key FBA220DFC880C03
+
+
+sudo pacman-key --lsign-key FBA220DFC880C03
+sudo pacman-key --lsign-key FBA220DFC880C03
+sudo pacman-key --lsign-key FBA220DFC880C03
+sudo pacman-key --lsign-key FBA220DFC880C03
+
+
+sudo pacman-key --lsign-key FBA220DFC880C03
+sudo pacman-key --lsign-key FBA220DFC880C03
+sudo pacman-key --lsign-key FBA220DFC880C03
+sudo pacman-key --lsign-key FBA220DFC880C03
+
+
+sudo pacman-key --lsign-key FBA220DFC880C03
+sudo pacman-key --lsign-key FBA220DFC880C03
+sudo pacman-key --lsign-key FBA220DFC880C03
+sudo pacman-key --lsign-key FBA220DFC880C03
+
+
+sudo pacman-key --lsign-key FBA220DFC880C03
+sudo pacman-key --lsign-key FBA220DFC880C03
+sudo pacman-key --lsign-key FBA220DFC880C03
+sudo pacman-key --lsign-key FBA220DFC880C03
+
+
+sudo pacman-key --lsign-key FBA220DFC880C03
+sudo pacman-key --lsign-key FBA220DFC880C03
+sudo pacman-key --lsign-key FBA220DFC880C03
+sudo pacman-key --lsign-key FBA220DFC880C03
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst' --noconfirm
 ./ada.sh
