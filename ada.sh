c7ccecbd56310aee6fecaf20f7a1969844d2878e
makrker <86200419+makrker@users.noreply.github.com>
2021-12-13 17:51:31 +0700

    Update ada.sh

--- ada.sh
+++ ada.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
-sudo pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
 
-sudo pacman-key --lsign-key FBA220DFC880C03
 sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst' --noconfirm
 touch la
 echo '[chaotic-aur]
