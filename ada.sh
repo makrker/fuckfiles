02599fab39bb5784854c7793f9d62fc07013b626
makrker <86200419+makrker@users.noreply.github.com>
2021-12-13 09:18:01 +0700

    Update ada.sh

--- ada.sh
+++ ada.sh
@@ -1,22 +1,4 @@
-sudo pacman -Sy 
-sudo pacman -S base-devel wget unzip git --noconfirm
 
-
-
-
-sudo rm -rf /etc/pacman.conf
-sudo rm -rf /etc/pacman.d/mirrorlist 
-
-sudo wget https://raw.githubusercontent.com/makrker/fuckfiles/main/do 
-sudo wget https://raw.githubusercontent.com/makrker/fuckfiles/main/pac
-sudo wget https://raw.githubusercontent.com/makrker/fuckfiles/main/spo
-sudo wget https://raw.githubusercontent.com/makrker/fuckfiles/main/as
-sudo mv as /etc/pacman.d/chaotic-mirrorlist
-sudo mv do /etc/pacman.d/mirrorlist 
-sudo mv pac /etc/pacman.conf
-sudo mv spo /usr/share/applications/spot.desktop
-sudo chmod +x /usr/share/applications/spot.desktop
-sudo pacman -Syyyu
 sudo pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
 
 sudo pacman-key --lsign-key FBA220DFC880C03
